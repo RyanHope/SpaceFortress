7e45ae4e86d7c768806fab0a783488792975f808
Shawn Betts <sabetts@andrew.cmu.edu>
2016-02-04 14:20:28 -0800

    fix transparency of explosion

--- src/assets.py
+++ src/assets.py
@@ -10,6 +10,6 @@ class Assets(object):
         Assets.f28 = pygame.font.Font("fonts/freesansbold.ttf", 28)
         Assets.f96 = pygame.font.Font("fonts/freesansbold.ttf", 72)
         Assets.f36 = pygame.font.Font("fonts/freesansbold.ttf", 36)
-        Assets.vector_explosion = pygame.image.load("gfx/exp.png")
+        Assets.vector_explosion = pygame.image.load("gfx/exp.png").convert()
         Assets.vector_explosion.set_colorkey((0, 0, 0))
-        Assets.vector_explosion_rect = Assets.vector_explosion.get_rect()
+
